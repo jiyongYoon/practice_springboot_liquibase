c66a496cbecae1c78f65c2b79f2b118bf8790148
jiyong.yoon <jyyoon0615@naver.com>
2023-08-28 10:09:34 +0900

    migration 환경분리
    - prod, local 등으로 배포 환경분리 적용

--- src/main/java/com/example/liquibase/LiquibaseApplication.java
+++ src/main/java/com/example/liquibase/LiquibaseApplication.java
@@ -1,13 +1,25 @@
 package com.example.liquibase;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiquibaseApplication {
+
+    @Value("${spring.datasource.url}")
+    private String url;
 
     public static void main(String[] args) {
         SpringApplication.run(LiquibaseApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner printUrl() {
+        return args -> {
+            System.out.println("Database URL: " + url);
+        };
+    }
 }
